a07a88afcf9ec6de5410ececf0d453ebade23913
Ryan Wey <rwey@ucsd.edu>
2021-01-16 22:23:16 -0800

    added Javascript finally!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-    initializePage();
+  initializePage();
 });
 
 function initializePage() {
-    $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+  $('#testjs').click(function(e) {
+    $('.jumbotron h1').text("Javascript is connected");
+    $("a.thumbnail").click(projectClick);
+  });
+}
+
+function projectClick(e) {
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
 }
